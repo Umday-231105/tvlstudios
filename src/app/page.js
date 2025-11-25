16852058c70ae6914958bbffd2b82c6f60abe8ca
Umday-231105 <uday231105@gmail.com>
2025-11-25 16:45:16 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -11,8 +11,7 @@ import {
   Cpu, 
   Aperture,
   MonitorPlay,
-  Github,
-  Twitter,
+  FiIntagram,
   Linkedin,
   ArrowRight,
   CheckCircle2,
@@ -579,7 +578,7 @@ export default function App() {
                 <div>
                   <h4 className="text-white font-bold mb-8 text-lg">Connect</h4>
                   <ul className="space-y-6 text-gray-400">
-                    <li><a href="#" className="hover:text-white transition-colors flex items-center gap-3"> Instagram</a></li>
+                    <li><a href="#" className="hover:text-white transition-colors flex items-center gap-3"><FiInstagram size={20}/> Instagram</a></li>
                     <li><a href="mailto:hello@tvl.studio" className="hover:text-white transition-colors mt-4 block">tvlstudioz@gmail.com</a></li>
                   </ul>
                 </div>
